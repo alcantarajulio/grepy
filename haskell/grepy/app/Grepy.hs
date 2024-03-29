885fd9c5863f5b65488930c513d055631b7ed307
alcantarajulio <julioalc21@gmail.com>
2024-03-29 15:17:29 -0300

    fix: remove duplicated main

--- haskell/grepy/app/Grepy.hs
+++ haskell/grepy/app/Grepy.hs
@@ -18,15 +18,3 @@ processLines (x:xs) = findMatches x : processLines xs
   where 
     findMatches 
 --TODO 
-
-
--- Main function that reads the file and prints matching lines
-main :: IO ()
-main = do
-  args <- getArgs
-  case args of
-    [filePath, pattern] -> do
-      contents <- readFile filePath
-      let finalMatches = findMatches contents pattern
-      mapM_ putStrLn finalMatches
-    _ -> putStrLn "Usage: grepy <file-path> <regex-pattern>"
