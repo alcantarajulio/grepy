093f7f5bda28478a76f485d9f1246d5e27bce8dc
Winicius Silva <winiciusab12@gmail.com>
2024-03-29 13:23:52 -0300

    feat: add setup script

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+WORK_DIR="$(pwd)/haskell/grepy"
+
+# Ensure ~/.local/bin is in PATH
+if [[ $(echo $PATH | grep -q "/.local/bin") ]]; then
+    echo "O diretório ~/.local/bin não está no seu PATH. Por favor, coloque-o."
+    exit 1
+fi
+
+cd $WORK_DIR
+stack build --copy-bins
+mv ~/.local/bin/grepy-exe ~/.local/bin/grepy
+chmod +x ~/.local/bin/grepy
+
