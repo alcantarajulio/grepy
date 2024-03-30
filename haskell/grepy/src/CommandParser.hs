12fee70740935f2c637b40f5599b19cd3828f658
alcantarajulio <julioalc21@gmail.com>
2024-03-30 00:13:47 -0300

    feat: add wordregexp implementation

--- haskell/grepy/src/CommandParser.hs
+++ haskell/grepy/src/CommandParser.hs
@@ -7,6 +7,7 @@ import Data.Maybe ( isNothing )
 import Grepy (grepy)
 import System.IO (readFile)
 import Count (countLines)
+import WordRegexp (wordRegExp)
 
 -- Mock function to test dispatch
 -- hello :: Maybe FilePath -> IO ()
@@ -16,9 +17,23 @@ dispatch :: Maybe String -> String -> Maybe String -> [String]
 dispatch (Just "--help") _ _ = usage
 dispatch (Just "-h") _ _ = usage
 dispatch _ _ Nothing = usage
+
 -- No flag
 dispatch Nothing pattern (Just content) = grepy pattern content
+
 -- Count
 dispatch (Just "--count") pattern (Just content) = [show (countLines (grepy pattern content))]
 dispatch (Just "-c") pattern (Just content) = [show (countLines (grepy pattern content))]
+
+WordRegexp
+dispatch (Just "--word-regexp") pattern (Just content) =
+    case wordRegExp pattern of
+        Just regex -> grepy regex content
+        Nothing -> ["Failed to generate word regex"]  -- or any other appropriate handling
+dispatch (Just "-w") pattern (Just content) =
+    case wordRegExp pattern of
+        Just regex -> grepy regex content
+        Nothing -> ["Failed to generate word regex"]  -- or any other appropriate handling
+        
+-- Se nenhuma das flags acima corresponder, exibe a mensagem de uso
 dispatch _ _ _ = usage
--- haskell/grepy/src/Utils.hs
+++ haskell/grepy/src/Utils.hs
@@ -6,13 +6,13 @@ import Data.List.Split (splitOn)
 
 usage :: [String]
 usage = splitOn "\n" "Usage: grepy [-c | --count] [-r | --recursive] \n\
-       \     [-w | --wordregex] [-e | --exclude] [-h | --help] <file>\n\ 
+       \     [-w | --word-regexp] [-e | --exclude] [-h | --help] <file>\n\ 
        \ \n\
 \ O grepy procura por casamento dos padrões nos arquivos ou string passadas como entrada para o programa.\n\
 \ \n\ 
 \ options: \n\
 \ -c, --count        Exibe a contagem dos padrões casados. \n\
 \ -r, --recursive    Procura pelo padrão nos arquivos do diretório passado como parâmetro, recursivamente. \n\
-\ -w, --wordregex    O padrão é buscado seguindo a palavra passada como parâmetro. \n\
+\ -w, --word-regexp  O padrão é buscado seguindo a palavra passada como parâmetro. \n\
 \ -e, --exclude      Exclui um arquivo da busca recursiva. Deve ser usada juntamente com `-r` \n\
 \ -h, --help         Mostra a mensagem de uso do programa."
