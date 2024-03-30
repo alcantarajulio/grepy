15b386c915babfe9e149b6861d7adbe3322db114
Francisco-Xiq <franciscopsf.contato@gmail.com>
2024-03-30 01:44:12 -0300

    refactor: main and parse of args

--- haskell/grepy/app/CommandParser.hs
+++ haskell/grepy/app/CommandParser.hs
@@ -1,5 +1,6 @@
 module CommandParser (
-    dispatch
+    dispatch,
+    dispatch2
 ) where
 
 import Utils (usage)
@@ -21,4 +22,9 @@ dispatch Nothing pattern (Just content) = grepy pattern content
 -- Count
 dispatch (Just "--count") pattern (Just content) = [show (countLines (grepy pattern content))]
 dispatch (Just "-c") pattern (Just content) = [show (countLines (grepy pattern content))]
+-- Recursive
+-- No match
 dispatch _ _ _ = usage
+
+dispatch2 :: Maybe String -> String -> String -> Maybe String-> [String]
+dispatch2 (Just "--recussive") pattern file_path (Just path) = usage
--- haskell/grepy/app/Utils.hs
+++ haskell/grepy/app/Utils.hs
@@ -1,8 +1,13 @@
 module Utils (
-    usage
+    usage, stdinVerify, isFlag, fileNotExits
 ) where
 
 import Data.List.Split (splitOn)
+import System.Posix.Terminal (queryTerminal)
+import Data.String (String)
+import System.Directory (doesFileExist)
+
+
 
 usage :: [String]
 usage = splitOn "\n" "Usage: grepy [-c | --count] [-r | --recursive] \n\
@@ -16,3 +21,15 @@ usage = splitOn "\n" "Usage: grepy [-c | --count] [-r | --recursive] \n\
 \ -w, --wordregex    O padrão é buscado seguindo a palavra passada como parâmetro. \n\
 \ -e, --exclude      Exclui um arquivo da busca recursiva. Deve ser usada juntamente com `-r` \n\
 \ -h, --help         Mostra a mensagem de uso do programa."
+
+stdinVerify :: IO Bool
+stdinVerify = not <$> queryTerminal 0
+
+isFlag :: String -> Bool
+isFlag flag = flag `elem` [ "--count", "-c", "--help", "-h",
+      "--wordregex",  "-w", "--recursive",  "-r",
+      "--recursive-exlcude",  "-e"]
+
+fileNotExits :: FilePath -> IO Bool
+fileNotExits path = doesFileExist path
+
