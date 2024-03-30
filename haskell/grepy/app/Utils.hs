18c2d5fe246f8b85178ff0142cd138a87e403cf3
Isaac Vicente <77455425+isaacvicente@users.noreply.github.com>
2024-03-29 23:43:58 -0300

    fix: correct option name
    the option for word regex is not `--wordregex` but
    rather `--word-regexp`

--- haskell/grepy/app/Utils.hs
+++ haskell/grepy/app/Utils.hs
@@ -1,17 +1,19 @@
-module Utils (
-    usage
-) where
+module Utils
+  ( usage,
+  )
+where
 
 usage :: IO ()
-usage = do 
-    putStrLn "Usage: grepy [-c | --count] [-r | --recursive] \n\
-       \     [-w | --wordregex] [-e | --exclude] [-h | --help] <file>\n\ 
-       \ \n\
-\ O grepy procura por casamento dos padrões nos arquivos ou string passadas como entrada para o programa.\n\
-\ \n\ 
-\ options: \n\
-\ -c, --count        Exibe a contagem dos padrões casados. \n\
-\ -r, --recursive    Procura pelo padrão nos arquivos do diretório passado como parâmetro, recursivamente. \n\
-\ -w, --wordregex    O padrão é buscado seguindo a palavra passada como parâmetro. \n\
-\ -e, --exclude      Exclui um arquivo da busca recursiva. Deve ser usada juntamente com `-r` \n\
-\ -h, --help         Mostra a mensagem de uso do programa."
+usage = do
+  putStrLn
+    "Usage: grepy [-c | --count] [-r | --recursive] \n\
+    \     [-w | --word-regexp] [-e | --exclude] [-h | --help] <file>\n\
+    \ \n\
+    \ O grepy procura por casamento dos padrões nos arquivos ou string passadas como entrada para o programa.\n\
+    \ \n\
+    \ options: \n\
+    \ -c, --count        Exibe a contagem dos padrões casados. \n\
+    \ -r, --recursive    Procura pelo padrão nos arquivos do diretório passado como parâmetro, recursivamente. \n\
+    \ -w, --word-regexp    O padrão é buscado seguindo a palavra passada como parâmetro. \n\
+    \ -e, --exclude      Exclui um arquivo da busca recursiva. Deve ser usada juntamente com `-r` \n\
+    \ -h, --help         Mostra a mensagem de uso do programa."
