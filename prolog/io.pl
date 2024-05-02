825e628c6f6607318ef2f84dc7f04fe5aee4e0ce
alcantarajulio <julioalc21@gmail.com>
2024-05-02 20:50:53 -0300

    feat: add simple parsing which read an archive

---
+++ prolog/io.pl
@@ -0,0 +1,20 @@
+:- use_module(library(pio)).
+:- use_module(library(dcg/basics)).
+
+:- initialization(go, main).
+
+quoted([]) --> eos.
+quoted([Line|Ls]) --> string(Chars),
+                      {append([0''|Chars], [0''], Quoted),
+                       atom_chars(Line, Quoted) },
+                      eol, quoted(Ls).
+
+go:-
+    once(phrase_from_file(quoted(Lines), 'data.txt')),
+
+    atomics_to_string(Lines, '\n', Out),
+
+    open('t.txt', write, Stream),
+    write(Stream, Out),
+    nl(Stream),
+    close(Stream).
