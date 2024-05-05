ba09f1d8ee70415f38728ec4a776f0c58e1c6b29
Francisco-Xiq <franciscopsf.contato@gmail.com>
2024-05-05 17:47:42 -0300

    fix: refazendo main

--- prolog/main.pl
+++ prolog/main.pl
@@ -7,35 +7,32 @@ handle_args([], Result) :-
     usage(Result).
 % grepy -help
 % grepy -h
+% grepy <pattern> stdin
 handle_args([Arg1], Result) :-
-    (isFlag(Arg1) -> dispatch(Arg1, Result);usage(Result)).
+    (from_stdin -> (stdin_reader(StdinReturn), writeln(StdinReturn));usage(Result)).
 
 % grepy <pattern> file_path
-% grepy <pattern> stdin
+% grepy --count <pattern> stdin
+% grepy -c <pattern> stdin
+% grepy -word-regexp <pattern> stdin
+% grepy -w <pattern> stdin
 handle_args([Arg1, Arg2], Result) :-
-    ((file_exists(Arg2) -> (go(Arg2, Out), dispatch(Arg1, Out, Result)); dispatch(Arg1, Arg2, Result)); usage(Result)).
+    (from_stdin -> (stdin_reader(StdinReturn),dispatch(Arg1, Arg2, StdinReturn, Result)); (file_exists(Arg2) -> go(Arg2,ReturnIO),dispatch(Arg1, ReturnIO, Result); usage(Result))),
+    writeln(ReturnIO).
+
 
 % grepy --count <pattern> file_path
-% grepy --count <pattern> stdin
 % grepy -c <pattern> file_path
-% grepy -c <pattern> stdin
 % grepy -word-regexp <pattern> file_path
-% grepy -word-regexp <pattern> stdin
 % grepy -w <pattern> file_path
-% grepy -w <pattern> stdin
 % grepy --recursive <pattern> dir_path
 % grepy -r <pattern> dir_path
 handle_args([Arg1, Arg2, Arg3], Result) :-
     (isFlag(Arg1) -> 
-    (verifyRecursivesCases(Arg1) ->
-        dispatchRecursive(Arg1, Arg2, Arg3, Result)
-        ; (file_exists(Arg3) -> 
-            go(Arg3, Out), dispatch(Arg1, Arg2, Out, Result)
-          ;  
-          )
-    )
-    ; usage(Result)
-    )
+        (file_exists(Arg3) ->
+            (go(Arg3, ResultIO), dispatch(Arg1,Arg2,ResultIO,Result)); (verifyRecursivesCases(Arg1) -> dispatchRecursive(Arg1, Arg2, Arg3, Result)
+        ; usage(Result)))
+    ; usage(Result)).
     
     % (isFlag(Arg1) -> 
     % ((is_recursive(Arg1) -> 
@@ -46,18 +43,17 @@ handle_args([Arg1, Arg2, Arg3], Result) :-
 % grepy --recursive-exclude <pattern> file_path dir_path
 % grepy -e <pattern> file_path dir_path
 handle_args([Arg1, Arg2, Arg3, Arg4], Result) :-
-% falta fazer verificações
     (dispatchRecurssiveExclude(Arg1,Arg2,Arg3,Arg4,Result);usage(Result)).
 
 main :-
-    % leitor(Result),
-    % writeln(Result).
-    
+  
     current_prolog_flag(argv, Argv),
     handle_args(Argv, Result),
     writeln(Result),
     halt.
 
+      % leitor(Result),
+    % writeln(Result).
 
 % leitor([Line|Tail]):-
 % read_line_to_string(user_input,Line),
--- prolog/utils.pl
+++ prolog/utils.pl
@@ -1,12 +1,15 @@
-:- module(utils, [file_exists/1, is_recursive/1, isFlag/1, verifyRecursivesCases/1, usage/1]).
+:- module(utils, [file_exists/1, isFlag/1, verifyRecursivesCases/1, usage/1, from_stdin/0, stdin_reader/1]).
+
+:- use_module(library(tty)).
 
 file_exists(File) :- 
     exists_file(File).
 
-is_recursive("--recursive").
 
-stdin_is_pipe :-
-    \+ tty(0).
+from_stdin :-
+    current_input(Input),
+    \+ stream_property(Input, tty(true)).  
+
 
 isFlag(Flag) :-
     member(Flag, ["--count", "-c", "--help", "-h",
@@ -27,3 +30,37 @@ options:
 -e, --recursive-exclude  Tem funcionamento similar a `--recursive`, porem desconsidera o arquivo passado.  
 -w, --word-regexp        O padrão é buscado seguindo a palavra passada como parâmetro.
 -h, --help               Mostra a mensagem de uso do programa.".
+
+% xor(true, false).
+% xor(false, true).
+
+% xor(A, B) :-
+%     dif(A, B).
+
+
+
+% stdin_reader(Lines) :-
+%     stream_lines(user_input, Lines).
+
+% stream_lines(In, Lines) :-
+%     read_string(In, _, Str),
+%     split_string(Str, "\n", "\n", Lines).
+
+
+stdin_reader(String) :-
+    stream_lines('teste.txt', String).
+
+stream_lines(FileName, String) :-
+    open(FileName, read, Stream),
+    read_lines(Stream, Lines),
+    atomic_list_concat(Lines, '\n', String),
+    close(Stream).
+
+read_lines(Stream, []) :-
+    at_end_of_stream(Stream).
+read_lines(Stream, [Line|Rest]) :-
+    \+ at_end_of_stream(Stream),
+    read_line_to_string(Stream, Line),
+    read_lines(Stream, Rest).
+
+
